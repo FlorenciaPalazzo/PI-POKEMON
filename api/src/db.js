07d42e055ab11c3ce595fab68ad036c477cab45b
FlorenciaPalazzo <florencia.e.palazzo@gmail.com>
2022-06-25 10:57:28 -0300

    para deploy 1

--- api/src/db.js
+++ api/src/db.js
@@ -39,7 +39,7 @@ let sequelize =
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
-// const basename = path.basename(__filename);
+// const basename = path.basename(__filename); 
 
 
 const modelDefiners = [];
